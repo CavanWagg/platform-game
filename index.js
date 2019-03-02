327ff0909be743e3ab2d680ab471ae247c1a3009
Cavan Wagg <waggx002@gmail.com>
2019-03-01 20:12:26 -0600

    drawing

--- index.js
+++ index.js
@@ -139,9 +139,6 @@ const levelChars = {
   v: Lava
 };
 
-let simpleLevel = new Level(simpleLevelPlan);
-console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
-
 // helper function to provide a succint way to create an element and give it some
 // attributes and child nodes
 
@@ -156,14 +153,97 @@ function elt(name, attrs, ...children) {
   return dom;
 }
 
-class DOMdisplay {
+// A display is created by giving it a parent element to which it should append itself
+// and a level object.
+
+class DOMDisplay {
   constructor(parent, level) {
     this.dom = elt('div', { class: 'game' }, drawGrid(level));
+    console.log(this.dom);
+    console.log(parent);
     this.actorLayer = null;
     parent.appendChild(this.dom);
   }
-
   clear() {
     this.dom.remove();
   }
 }
+
+// scale constant gives the number of pixels that a single unit takes up on the screen
+const scale = 20;
+
+function drawGrid(level) {
+  return elt(
+    'table',
+    {
+      class: 'background',
+      style: `width: ${level.width * scale}px`
+    },
+    ...level.rows.map(row =>
+      elt(
+        'tr',
+        { style: `height: ${scale}px` },
+        ...row.map(type => elt('td', { class: type }))
+      )
+    )
+  );
+}
+
+function drawActors(actors) {
+  return elt(
+    'div',
+    {},
+    ...actors.map(actor => {
+      let rect = elt('div', { class: `actor ${actor.type}` });
+      rect.style.width = `${actor.size.x * scale}px`;
+      rect.style.height = `${actor.size.y * scale}px`;
+      rect.style.left = `${actor.pos.x * scale}px`;
+      rect.style.top = `${actor.pos.y * scale}px`;
+      return rect;
+    })
+  );
+}
+
+// the background is drawn as a <table> element. each row of the grid is turned into a table row
+
+// display a given state, remove the old actor graphics, then redraw the actors in their new positions.
+//
+DOMDisplay.prototype.syncState = function(state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
+
+// find the player's position and update the wrapping element's scroll position.
+
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width / 3;
+
+  // The viewport
+  let left = this.dom.scrollLeft,
+    right = left + width;
+  let top = this.dom.scrollTop,
+    bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+  if (center.x < left + margin) {
+    this.dom.scrollLeft = center.x - margin;
+  } else if (center.x > right - margin) {
+    this.dom.scrollLeft = center.x + margin - width;
+  }
+  if (center.y < top + margin) {
+    this.dom.scrollTop = center.y - margin;
+  } else if (center.y > bottom - margin) {
+    this.dom.scrollTop = center.y + margin - height;
+  }
+};
+console.log(document.body);
+let simpleLevel = new Level(simpleLevelPlan);
+let display = new DOMDisplay(document.body, simpleLevel);
+display.syncState(State.start(simpleLevel));
